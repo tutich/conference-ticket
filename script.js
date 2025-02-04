7f514742dd5149dfb4d3232a3c39c8c59a597a4f
tutich <pkoinange02@gmail.com>
2025-02-04 16:49:08 +0300

    styles

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("generate-ticket").addEventListener("click", function () {
+
+    // Get inputs
+    const fullName = document.getElementById("full-name");
+    const email = document.getElementById("email");
+    const userName = document.getElementById("username");
+    
+    // validate inputs
+    if {
+
+    }
+
+})
